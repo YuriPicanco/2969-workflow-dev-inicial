bd2ed6228cfd2be0e8bec799eb9e32c89547fb36
D.Souza <yuri.s.picanco@gmail.com>
2024-06-01 18:16:48 -0400

    feat: :test_tube: add: feature flag para /eventos e testes de /eventos

--- src/controllers/eventosController.js
+++ src/controllers/eventosController.js
@@ -1,15 +1,19 @@
 import Evento from "../models/evento.js";
 
 class EventosController {
-  static async liberaAcessoEventos() {}
+  static liberaAcessoEventos = () => process.env.EVENTO_FLAG === "true";
 
   static async listarEventos(req, res) {
-    try {
-      const resultado = await Evento.pegarEventos();
+    if (EventosController.liberaAcessoEventos()) {
+      try {
+        const resultado = await Evento.pegarEventos();
 
-      return res.status(200).json(resultado);
-    } catch (err) {
-      return res.status(500).json(err.message);
+        return res.status(200).json(resultado);
+      } catch (err) {
+        return res.status(500).json(err.message);
+      }
+    } else {
+      return res.status(404).send();
     }
   }
 }
